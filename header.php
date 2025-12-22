8f1854385ed4e9d0abf6bae14088a9f8714d0510
James Bumanlag <jamesbumanlag@gmail.com>
2025-12-23 09:31:31 +1000

    added search and login button

--- header.php
+++ header.php
@@ -1,15 +1,27 @@
 <header class="border">
     <nav>
-        <div class="title-page"><img src="images/virtual.png" alt="BTech Logo-image">
-            <h1>BTech Solution</h1>
-        </div>
-        <ul>
-            
-            <li><a href="#">Services</a></li>
-            <li><a href="#gallery">Gallery</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#">Contact</a></li>
+        <div class="title-page">
+            <div class="logo-title">
+                <img src="images/virtual.png" alt="BTech Logo-image">
+                <h1>BTech Solution</h1>
+            </div>
+        
+            <ul>
+                
+                <li><a href="#">Services</a></li>
+                <li><a href="#gallery">Gallery</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#">Contact</a></li>
 
-        </ul>
+            </ul>
+        </div>
+
+        <div class="login-search">
+            <input type="search" placeholder="Search..." class="searchInput">
+            <button name="search" class="searchButton">Go</button>
+            <button name="login" class="loginButton">Login</button>
+        </div>
+
+
     </nav>
 </header>
